c6342fa6ebc559cb36e4eda19b46b10110f9fb5b
Fairy-chen <44216723+DevinChenPeng@users.noreply.github.com>
2024-09-24 17:34:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -308,9 +308,7 @@ def get_time():
     """
     获取时间戳
     """
-    url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-    response = requests.get(url, headers=headers).json()
-    t = response['data']['t']
+    t = int(time.time())
     return t
 
 
